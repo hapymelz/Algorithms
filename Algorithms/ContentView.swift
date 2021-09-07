2f0567f7486e205f15b74fab572c8f153070bece
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-07 09:35:30 -0600

    created new repo.

---
+++ Algorithms/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Algorithms
+//
+//  Created by Numbers, Katherine on 9/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .bold()
+            .foregroundcolor(.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Algorithms/Resources/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Numbers, Katherine on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

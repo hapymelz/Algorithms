f7687c0faec23439b5e82132815ce688fb2693b8
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-07 09:36:00 -0600

    tested to see if repo was made correctly.

--- Algorithms/ContentView.swift
+++ Algorithms/ContentView.swift
@@ -11,8 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-            .bold()
-            .foregroundcolor(.black)
+            
     }
 }
 

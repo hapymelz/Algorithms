4915922294891cc431d5a519d228a5fc56623668
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-09 08:30:09 -0600

    moved my function and fixed some errors.

--- Algorithms/AlgorithmView.swift
+++ Algorithms/AlgorithmView.swift
@@ -25,30 +25,33 @@ struct AlgorithmView: View
 struct AlgorithmView_Previews: PreviewProvider
 {
     let algorithmTitle : String = "Code Algorithm"
-    private func setupAlgorithm() -> String
-    {
-        let algorithmTitle : String = "Paste text to a screen in XCode"
-        
-        let stepOne : String =  "Create an app, and delete the following portion: 'Text(Hello World)'"
-        let stepTwo : String = "Underneath “var body: some View { “, type “Vstack { “, it should be indented because it is in the function."
-        let stepThree : String = "Underneath “VStack { “, return indented, type “text(“Your text here_”)"
-        let stepFour : String = "You can change the text it outputs as long as the text is between the parentheses and within the quotation marks."
-        
-        let algorithm = [title, stepOne, stepTwo, stepThree, stepFour]
-        
-        var algorithmDescription = algorithmTitle
-        
-        for step in algorithm
-        {
-            let bullet : String = "🎮"
-            let bulletStep : 
-        }
-        
-      //  for
-        return stepTwo
-    }
+   
+    
     static var previews: some View
     {
         AlgorithmView()
     }
+}
+private func setupAlgorithm() -> String
+{
+    let algorithmTitle : String = "Paste text to a screen in XCode"
+    
+    let stepOne : String =  "Create an app, and delete the following portion: 'Text(Hello World)'"
+    let stepTwo : String = "Underneath “var body: some View { “, type “Vstack { “, it should be indented because it is in the function."
+    let stepThree : String = "Underneath “VStack { “, return indented, type “text(“Your text here_”)"
+    let stepFour : String = "You can change the text it outputs as long as the text is between the parentheses and within the quotation marks."
+    
+    var algorithm = [algorithmTitle, stepOne, stepTwo, stepThree, stepFour]
+    
+    let algorithmDescription = algorithmTitle
+    
+    for step in algorithm
+    {
+        let bullet : String = "🎮"
+        let bulletStep : String = "\n\(bullet) \(step)"
+        
+        algorithm.append(bulletStep)
+    }
+    
+    return algorithmDescription
 }
